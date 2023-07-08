9e5fff969b4212c12106b2f6e43c283b5a7e4782
JoseDaniel <jose.a.a.daniel@gmail.com>
2023-07-08 14:18:41 +0100

    Created details activity and corrected some item_row layout bugs

---
+++ app/src/main/java/com/example/cimbsenews/DetailsPostActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cimbsenews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailsPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_post)
+    }
+}
