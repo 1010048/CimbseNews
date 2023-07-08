d5dd1184ac44335f5d779e6b66026bb175093127
JoseDaniel <jose.a.a.daniel@gmail.com>
2023-07-08 15:39:03 +0100

    Created method to fetch the data from the server

--- app/src/main/java/com/example/cimbsenews/api/ApiService.kt
+++ app/src/main/java/com/example/cimbsenews/api/ApiService.kt
@@ -1,4 +1,11 @@
 package com.example.cimbsenews.api
 
+import com.example.cimbsenews.response.PostListResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+    @GET("/endpoint")
+    fun getPosts(@Query("page") page : Int) : Call<PostListResponse>
 }
